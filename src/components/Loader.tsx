ee3e4404e91ae080044ab8118cfff1e60d3bb61e
awadg8 <awadheshg798@gmail.com>
2025-02-28 16:27:25 +0530

    second commit

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const Loader = ({ color = "white" }) => {
+const Loader = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-white">
       <motion.div
